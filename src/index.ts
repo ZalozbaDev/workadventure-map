027e3a44438cd33e286124ced380773748c64ea3
zobadaniel <87025786+zobadaniel@users.noreply.github.com>
2021-11-09 11:55:39 +0000

    Deploying to gh-pages from master @ 92450225c4cf906ccd4d6745dc7a43263e37960c 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
+import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+
+// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
+bootstrapExtra().catch(e => console.error(e));
+
 let currentPopup: any = undefined;
 const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+const time = today.getHours() + ":" + today.getMinutes();
 
-WA.onEnterZone('clock', () => {
-    currentPopup =  WA.openPopup("clockPopup","It's " + time,[]);
+WA.room.onEnterZone('clock', () => {
+    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
 })
 
-WA.onLeaveZone('clock', closePopUp)
+WA.room.onLeaveZone('clock', closePopUp)
 
 function closePopUp(){
     if (currentPopup !== undefined) {
